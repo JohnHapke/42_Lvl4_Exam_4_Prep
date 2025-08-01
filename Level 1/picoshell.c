fccd0be2393178ccd24e3c231f0e4307a009dd51
johnhapke <mail@john-hapke.de>
2025-08-01 12:39:55 +0200

    V01.02 - picoshell.c coded, tested & debugged

--- Level 1/picoshell.c
+++ Level 1/picoshell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   picoshell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jhapke <jhapke@student.42.fr>              +#+  +:+       +#+        */
+/*   By: johnhapke <johnhapke@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 10:17:58 by jhapke            #+#    #+#             */
-/*   Updated: 2025/07/31 11:55:42 by jhapke           ###   ########.fr       */
+/*   Updated: 2025/08/01 12:39:02 by johnhapke        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ ___
 
 Write the following function:
 
-int    picoshell(char *cmds[]);
+int    picoshell(char **cmds[]);
 
 The goal of this function is to execute a pipeline. It must execute each
 commands [sic] of cmds and connect the output of one to the input of the
@@ -45,18 +45,20 @@ squblblb/
 #include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int	picoshell(char *cmds[])
+int	picoshell(char **cmds[])
 {
 	int		fd[2];
 	int		i;
 	pid_t	pid;
 	int		prev_fd;
-	int		j;
+	int		status;
 
-	j = -1;
-	prev_fd = -1;
 	i = -1;
+	prev_fd = -1;
+	fd[0] = -1;
+	fd[1] = -1;
 	while (cmds[++i])
 	{
 		if (cmds[i + 1] != NULL)
@@ -67,46 +69,103 @@ int	picoshell(char *cmds[])
 		pid = fork();
 		if (pid < 0)
 		{
-			close(fd[0]);
-			close(fd[1]);
+			if (cmds[i + 1] != NULL)
+			{
+				close(fd[0]);
+				close(fd[1]);
+				if (prev_fd != -1)
+					close(prev_fd);
+			}
 			exit(1);
 		}
 		if (pid == 0)
 		{
 			if (i != 0)
-				dup2(prev_fd, STDIN_FILENO);
+				if (dup2(prev_fd, STDIN_FILENO) == -1)
+				{
+					if (cmds[i + 1] != NULL)
+					{
+						close(fd[0]);
+						close(fd[1]);
+						if (prev_fd != -1)
+							close(prev_fd);
+					}
+					exit(1);
+				}
 			if (cmds[i + 1] != NULL)
 			{
-				dup2(fd[1], STDOUT_FILENO);
+				if (dup2(fd[1], STDOUT_FILENO) == -1)
+				{
+					close(fd[0]);
+					close(fd[1]);
+					if (prev_fd != -1)
+						close(prev_fd);
+					exit(1);
+				}
 				close(fd[0]);
 			}
-			close(fd[1]);
-			execvp(&cmds[i][0], &cmds[i]);
+			if (fd[1] != -1)
+				close(fd[1]);
+			execvp(cmds[i][0], cmds[i]);
 			exit(1);
 		}
 		else
 		{
 			if (prev_fd != -1)
 				close(prev_fd);
-			prev_fd = fd[0];
-			close(fd[1]);
+			if (cmds[i + 1] != NULL)
+			{
+				prev_fd = fd[0];
+				close(fd[1]);
+			}
 		}
 	}
-	while (wait(NULL) > 0)
-		;
+	while (wait(&status) > 0)
+	{
+		if (WIFEXITED(status))
+		{
+			if (WEXITSTATUS(status) != 0)
+			return (1);
+		}
+		else
+			return (1);
+	}
 	return (0);
 }
 
-/*
-int	main(int ac, char **av)
+/*int	main(void)
 {
 	int	res;
 
-	if (ac < 2)
-	{
-		printf("usage\n");
-		return (1);
-	}
-	res = picoshell(av);
+	printf("test1:");
+	char *cmd1[] = {"ls", NULL};
+	char *cmd2[] = {"grep", "picoshell", NULL};
+	char **cmds1[] = {cmd1, cmd2, NULL};
+	res = picoshell(cmds1);
+	printf("\ntest2:");
+	char *cmd3[] = {"ls", "-l", NULL};
+	char **cmds2[] = {cmd3, NULL};
+	res = picoshell(cmds2);
+	printf("\ntest3:");
+	char *cmd4[] = {"echo", "hello world", NULL};
+	char *cmd5[] = {"cat", NULL};
+	char *cmd6[] = {"wc", "-w", NULL};
+	char **cmds3[] = {cmd4, cmd5, cmd6, NULL};
+	res = picoshell(cmds3);
+	printf("\ntest4:");
+	char *cmd7[] = {"/bin/ls", NULL};
+	char *cmd8[] = {"/usr/bin/grep", "picoshell", NULL};
+	char **cmds4[] = {cmd7, cmd8, NULL};
+	res = picoshell(cmds4);
+	printf("\ntest5:");
+	char *cmd9[] = {"echo", "test123", NULL};
+	char *cmd10[] = {"cat", NULL};
+	char **cmds5[] = {cmd9, cmd10, NULL};
+	res = picoshell(cmds5);
+	printf("\ntest6:");
+	char *cmd11[] = {"ls", NULL};
+	char *cmd12[] = {"wc", "-l", NULL};
+	char **cmds6[] = {cmd11, cmd12, NULL};
+	res = picoshell(cmds6);
 	return (res);
-} */
+}*/
